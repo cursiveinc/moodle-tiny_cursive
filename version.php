7c29818d72c0df6da333785819ba6061b9c581ce
Tareq-Adnan <adnantareq2@gmail.com>
2026-01-29 20:24:59 +0600

    version update

--- version.php
+++ version.php
@@ -29,6 +29,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_cursive';
 $plugin->release = '2.1.2';
-$plugin->version = 2025121901;
+$plugin->version = 2025122000;
 $plugin->requires = 2022041912;
 $plugin->maturity = MATURITY_STABLE;
