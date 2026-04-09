41da828526cc06cdff4101cdc1ed288971a21621
Tareq-Adnan <adnantareq2@gmail.com>
2026-04-09 16:37:17 +0600

    fixed forum reply button action

--- amd/src/append_fourm_post.js
+++ amd/src/append_fourm_post.js
@@ -65,32 +65,6 @@ define(["jquery", "core/ajax", "core/str", "core/templates", "./replay", "./anal
         },
         getToken: function(scoreSetting, showcomment, hasApiKey) {
             $('#page-mod-forum-discuss').find("article").get().forEach(function(entry) {
-                var replyButton = $('a[data-region="post-action"][title="Reply"]');
-                if (replyButton.length > 0) {
-                    replyButton.on('click', function(event) {
-                        var isTeacher = $('#body').hasClass('teacher_admin');
-                        if (!isTeacher) {
-                            event.preventDefault();
-                            var url = $(this).attr('href');
-
-                            var urlParts = url.split('#');
-                            var baseUrl = urlParts[0];
-                            var hash = urlParts.length > 1 ? '#' + urlParts[1] : '';
-
-                            if (baseUrl.indexOf('setformat=') > -1) {
-                                baseUrl = baseUrl.replace(/setformat=\d/, 'setformat=1');
-                            } else if (baseUrl.indexOf('?') > -1) {
-                                baseUrl += '&setformat=1';
-                            } else {
-                                baseUrl += '?setformat=1';
-                            }
-                            var finalUrl = baseUrl + hash;
-
-                            window.location.href = finalUrl;
-                        }
-
-                    });
-                }
 
                 var ids = $("#" + entry.id).data("post-id");
                 var cmid = M.cfg.contextInstanceId;
